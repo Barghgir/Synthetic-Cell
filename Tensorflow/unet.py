5bfa6d8adc08c1dfb1a18005bd96d0732ecc2f49
Ali Reza Barghgir <61283972+Barghgir@users.noreply.github.com>
2022-12-11 10:44:58 +0330

    upload unet file

---
+++ Tensorflow/unet.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import keras.layers as Layers
+from keras.layers import Conv2D, Input, BatchNormalization
+
+class Unet(Layers):
+    def __init__():
+        super.__init__(self, Unet)
+        
