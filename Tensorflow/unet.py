bca88df6fa006971366d1297685c87087237b625
Barghgir <alireza.barghgir@gmail.com>
2022-12-19 13:34:33 +0330

    Create First unet model

--- Tensorflow/unet.py
+++ Tensorflow/unet.py
@@ -1,8 +1,82 @@
 import tensorflow as tf
 import keras.layers as Layers
-from keras.layers import Conv2D, Input, BatchNormalization
+from keras.layers import Conv2D, Input, BatchNormalization, ReLU, MaxPooling2D, GlobalAveragePooling2D, Conv2DTranspose
+from keras import Model
+# from keras.layers.merge import concatenate
+# from keras.layers import Concatenate
+# concatenate([u9, c1], axis=3)
+class Downward_block(Layers):
+    def __init__(self, num_filters):
+        super('Downward_block', self).__init__(Downward_block)
+        self.conv3 = Conv2D(num_filters, (3, 3), padding='same', activation='relu')
+        self.maxpool = MaxPooling2D((2, 2))
+
+    def call(self, x):
+        x = self.conv3(x)
+        x = self.conv3(x)
+        x = self.maxpool(x)
+        return x
+
+
+class Upward_block(Layers):
+    def __init__(self, conv3_filters, upconv_filters):
+        super("Upward_block", self).__init__(Upward_block)
+        self.conv3_1 = Conv2D(conv3_filters, (3, 3), padding='same', activation='relu')
+        self.conv3_2 = Conv2D(conv3_filters, (3, 3), padding='same', activation='relu')
+
+        self.upconv = Conv2DTranspose(upconv_filters, (2, 2), activation='relu')
+
+    def call(self, x):
+        x = self.upconv(x)
+        x = self.conv3_1(x)
+        x = self.conv3_2(x)
+        
+        return x
+
 
 class Unet(Layers):
-    def __init__():
-        super.__init__(self, Unet)
+    def __init__(self):
+        super('Unet', self).__init__(Unet)
+
+        self.down1 = Downward_block(64)
+        self.down2 = Downward_block(128)
+        self.down3 = Downward_block(256)
+        self.down4 = Downward_block(512)
+
+        self.conv3 = Conv2D(1024, (3, 3), padding='same', activation='relu')
+
+        self.up4 = Upward_block(512)
+        self.up3 = Upward_block(256) 
+        self.up2 = Upward_block(128) 
+        self.up1 = Upward_block(64) 
+
+        self.conv1 = Conv2D(1, (1, 1), padding='same', activation='relu')
+
+    def call(self, x):
+        x1 = self.down1(x)
+        x2 = self.down2(x1)
+        x3 = self.down3(x2)
+        x4 = self.down4(x3)
+
+        x = self.conv3(x4)
+        x = self.conv3(x)
+
+
+
+
+
+        
+
+
+
+    # def build(self, input_shape):
+    #     x = self.input_layer()
+    #     output = self.call(x)
+    #     return Model(inputs=x, outputs=output)
+
+    
+    # def build(self, raw_shape):
+    #     x = Input(shape=(None, raw_shape), ragged=True)
+    #     return tf.keras.Model(inputs=[x], outputs=self.call(x)) 
+
         
