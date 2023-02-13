c2f9c5b7a3d425866030951290704da28cbc97ee
Barghgir <alireza.barghgir@gmail.com>
2023-02-13 20:28:28 +0330

    import generators from Generators file

--- Tensorflow/number_01.py
+++ Tensorflow/number_01.py
@@ -9,6 +9,7 @@ from glob import glob
 import matplotlib.pyplot as plt
 import pandas as pd
 import tensorflow as tf
+from Generators import gen_pairs_test, gen_pairs_train
 
 from skimage.io import imread, imshow
 from skimage.transform import resize
